28c58638c7acb8ae28d67ff681c7418d97cf286f
Jean-Lionel <nijeanlionel@gmailcom>
2025-12-28 08:23:44 +0200

    create invoinc invoice pointer table

--- ionic-app/capacitor.config.ts
+++ ionic-app/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.shoopingapp',
-  appName: 'Shooping App',
+  appId: 'com.clsky.clskMobileApp',
+  appName: 'clskMobileApp',
   webDir: 'build'
 };
 
